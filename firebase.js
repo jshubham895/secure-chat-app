e997e102978b31d639e5ae5e7b669ed25abc9b4b
jshubham895 <jshubham895@gmail.com>
2021-09-18 23:15:18 +0530

    nice going

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCbVbzYe6zbRjqQbx4Xzftaq8_RMRMVPhU",
+	authDomain: "chat-app-25205.firebaseapp.com",
+	projectId: "chat-app-25205",
+	storageBucket: "chat-app-25205.appspot.com",
+	messagingSenderId: "226897523158",
+	appId: "1:226897523158:web:cffe8d532264b255c0f477",
+	measurementId: "G-M4DPWHDMSR"
+};
---
+++ screens/LoginScreen.js
@@ -0,0 +1,67 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+
+const LoginScreen = ({ navigation }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const signIn = () => {};
+
+	return (
+		<KeyboardAvoidingView behaviour="padding" style={styles.container}>
+			<StatusBar style="light" />
+			<Image
+				style={{ width: 200, height: 200 }}
+				source={{
+					uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
+				}}
+			/>
+			<View style={styles.inputContainer}>
+				<Input
+					placeholder="Email"
+					autoFocus
+					type="email"
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+				/>
+				<Input
+					placeholder="Password"
+					secureTextEntry
+					type="password"
+					value={password}
+					onChangeText={(password) => setPassword(password)}
+				/>
+			</View>
+			<Button containerStyle={styles.button} onPress={signIn} title="Login" />
+			<Button
+				containerStyle={styles.button}
+				title="Register"
+				type="outline"
+				onPress={() => navigation.navigate("Register")}
+			/>
+			<View style={{ height: 100 }} />
+		</KeyboardAvoidingView>
+	);
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 10,
+		backgroundColor: "white"
+	},
+	inputContainer: {
+		width: 300
+	},
+	button: {
+		width: 200,
+		marginTop: 10
+	}
+});
